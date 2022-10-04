2a6f003872de1c8d084d5e76fd840ff088b1f014
Richard Stenstrom <richard_stenstrom@hotmail.com>
2022-10-04 19:03:55 -0400

    Initial Commit

---
+++ 2 Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Button Challenge
+//
+//  Created by Richard Stenstrom on 2022-10-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
